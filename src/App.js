bf008ddd62633924b7533fd72614e104ef2a9b46
Edward Ho <34835565+edwardmfho@users.noreply.github.com>
2022-06-23 00:58:32 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+// https://7leu5b4x2b.execute-api.ap-southeast-1.amazonaws.com/dev/arithmetic/add
+// https://7leu5b4x2b.execute-api.ap-southeast-1.amazonaws.com/dev/arithmetic/subtract
+// https://7leu5b4x2b.execute-api.ap-southeast-1.amazonaws.com/dev/arithmetic/multiply
+// https://7leu5b4x2b.execute-api.ap-southeast-1.amazonaws.com/dev/arithmetic/divide
+
+export default function App() {
+  return (
+    <div className="App">
+      <h5>Hello CodeSandbox</h5>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
